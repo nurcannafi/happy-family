5f70366fa6cd9d484b19da6409ab168b05e7a467
anarouis <anarftliyev@gmail.com>
2024-10-10 15:17:29 +0400

    hw-02-anar-family_methods

--- src/main/java/hw02/Family.java
+++ src/main/java/hw02/Family.java
@@ -3,6 +3,9 @@ package hw02;
 import hw01.Human;
 import hw01.Pet;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Family {
 
     private Human mother;
@@ -57,5 +60,55 @@ public class Family {
 
     public void setPet(Pet pet) {
         this.pet = pet;
+    }
+
+    public boolean deleteChild(Human child) {
+        boolean isDeleted = false;
+        for (int i = 0; i < children.length; i++) {
+            if (children[i].equals(child)) {
+                children[i] = null;
+                isDeleted = true;
+            }
+        }
+        return isDeleted;
+    }
+
+    public Human[] addChild(Human child) {
+        Human[] newChildren = Arrays.copyOf(children, children.length + 1);
+        newChildren[children.length] = child;
+        children = newChildren;
+        return children;
+    }
+
+    public int countFamily() {
+        return children.length + 2;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(father, mother);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Family that = (Family) obj;
+        return Objects.equals(father, that.father) &&
+                Objects.equals(mother, that.mother);
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "mother=" + (mother != null ? mother.toString() : "none") +
+                ", father=" + (father != null ? father.toString() : "none") +
+                ", children=" + Arrays.toString(children) +
+                ", pet=" + (pet != null ? pet.toString() : "none") +
+                '}';
     }
 }
