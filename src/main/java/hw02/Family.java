b91b06f3d3750adb7354ff75c35aed629252d1c7
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-11 17:35:19 +0400

    redefined finalize methods

--- src/main/java/hw02/Family.java
+++ src/main/java/hw02/Family.java
@@ -56,4 +56,12 @@ public class Family {
         this.pet = pet;
     }
 
+    @SuppressWarnings({"deprecation", "removal"})
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Family object is being removed: " + this);
+        super.finalize();
+    }
+
 }
